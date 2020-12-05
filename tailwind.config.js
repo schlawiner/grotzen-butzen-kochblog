c0349379a400bde8cf2076b025bcef3b6423b828
Tina Trillitzsch <ttr@leibniz-psychology.org>
2020-12-05 20:40:20 +0100

    Update Tailwind to 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto', 'ui-sans-serif', 'system-ui'],
+      'serif': ["'Crimson Pro'", "'PT Serif'", 'Yrsa', 'ui-serif', 'Georgia'],
+     },
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {
+      backgroundColor: ['odd', 'even'],
+    }
+  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
